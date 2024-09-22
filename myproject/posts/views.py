3d8a687d587af6447e5ac34fcc3a56937ebb3697
Aravinth-S-731 <aravinth7871867225@gmail.com>
2024-09-22 19:00:24 +0530

    Unsure

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 from .models import Post
 
 # Create your views here.
 def posts_list(request):
     posts = Post.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html', { 'posts' : posts})
+
+def post_page(request, slug):
+    # return HttpResponse(slug)
+    post = Post.objects.get(slug = slug)
+    print(post)
+    return render(request, 'posts/post_page.html', { 'post' : post })
